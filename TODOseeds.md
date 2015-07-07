5f237cd5d093af19d47008abb0e6988d7ef38aac
F3PiX <F3PiXnl@gmail.com>
2015-07-07 20:14:35 +0200

    Updated seeds.rb with review comments; ready to 'rake it'

---
+++ TODOseeds.md
@@ -0,0 +1,20 @@
+Populating seeds with faker users
+(as discussed with André)
+
+
+To do: 	#latest first 
+
+[ ] Write test to check JobOffers 
+[ ] Run rake to populate db with fake JobOffers
+[V] In seeds.rb : add code for Job Offer model
+[V] Add FFaker gem #already in gem file :development group 
+
+
+
+ *written in vi :-) 
+
+
+
+
+
+ 
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,47 +7,52 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-# Temp commented out seeds from upstream repo
+# Temp commented out: seeds from upstream repo
   # role = Role.create!([{name: 'organizer'}])
   # user = User.create!([{name: 'Wolverine', roles: role, github_handle: 'Adminis28'}])
 
-require 'FFaker' #TODO: do I need this?
 
-# JobOffer.create!( title: "Example Job Title",
-#                   company_name: "Example Company",
-#                   contact_name: "Example Name",
-#                   description: "Interesting details",
-#                   duration: "12 months",
-#                   location: "US",
-#                   misc_info: "tralala",
-#                   paid: true,
-#                   url: "http://www.google.com",
-#                   activated: true,
-#                   activated_at: Time.zone.now)
+4.times do
+   title = FFaker::Job.title
+   company_name = FFaker::Company.name
+   contact_name = FFaker::Name.name
+   description = FFaker::HipsterIpsum.phrase
+   duration = "#{Random.new.rand(3..24)} months"  #type is string or like this: "#{[3,6,12,24].sample} months"
+   location = FFaker::Address.country             #type is string
+   misc_info = FFaker::Company.bs                 #type is string
+   paid = FFaker::Boolean.random                  #type is bool
+   url = FFaker::Internet.http_url                #url is string
+   activated = true
+   activated_at = Time.zone.now
 
-    4.times do
-       title = FFaker::Job.title
-       company_name = FFaker::Company.name
-       contact_name = FFaker::Name.name
-       description = FFaker::HipsterIpsum.phrase
-       duration = "#{Random.new.rand(3..24)} months"  #type is string
-       location = FFaker::Address.country             #type is string
-       misc_info = FFaker::Company.bs                 #type is string
-       paid = FFaker::Boolean.random                  #type is bool
-       url = FFaker::Internet.http_url                #url is string
-       activated = true
-       activated_at = Time.zone.now
+  JobOffer.create!(title: title,
+     company_name: company_name,
+     contact_name: contact_name,
+     description: description,
+     duration: duration,
+     location: location,
+     misc_info: misc_info,
+     paid: true,
+     url: url,
+     activated: true,
+     activated_at: Time.zone.now)
+ end
 
-      JobOffer.create!(title: title,
-                       company_name: company_name,
-                       contact_name: contact_name,
-                       description: description,
-                       duration: duration,
-                       location: location,
-                       misc_info: misc_info,
-                       paid: true,
-                       url: url,
-                       activated: true,
-                       activated_at: Time.zone.now)
-     end
+# TEMP comments, during development (and coaching)
+# One extra, to check if non activated offer will not show up on page
+# TODO : Before I uncomment this, I want to know if seeding works; if so,
+# I will if test if the local vars can live outside the method
+# and then add the NonActivated JobOffer
+
+# JobOffer.create!(title: "NonActivated JobOffer",
+#      company_name: company_name,
+#      contact_name: contact_name,
+#      description: description,
+#      duration: duration,
+#      location: location,
+#      misc_info: misc_info,
+#      paid: true,
+#      url: url,
+#      activated: false,
+#      activated_at: Time.zone.now)
 
