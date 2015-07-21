d2bc7d82a2557d2392705bd832c9dfb37302640d
F3PiX <F3PiXnl@gmail.com>
2015-07-21 22:14:16 +0200

    Adding :unscope, trying to correct number of users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,9 @@ class UsersController < ApplicationController
     @users = @users.with_assigned_roles if Time.now.utc > (current_season.starts_at || Date.new)
     @users = @users.with_role(params[:role]) if params[:role].present? && params[:role] != 'all'
     @users = @users.with_interest(params[:interest]) if params[:interest].present? && params[:interest] != 'all'
+   # @users = @users.where.not(xxx)
+
+
   end
 
   def show
